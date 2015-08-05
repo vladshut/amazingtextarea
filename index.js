12b96004cee7506874ee851a063124b4b599173e
Vladyslav Shut <fred.shut@mail.ru>
2015-08-05 23:45:48 +0300

    PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
